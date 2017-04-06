1bc2a57f13b99bf53c1cd782ecc5a02d77bb3d53
simon <seu_syh@163.com>
2017-04-06 22:16:28 +0800

    完成了 Emit 最终匹配关键串类的编写
    Signed-off-by: simon <seu_syh@163.com>

--- src/main/java/segmentation/Trie/Emit.java
+++ src/main/java/segmentation/Trie/Emit.java
@@ -2,7 +2,23 @@ package segmentation.Trie;
 
 /**
  * Created by Administrator on 2017/4/6.
+ * 模式串匹配结果
  */
 public class Emit {
-    
+
+    /**
+     * 匹配到的关键串
+     */
+    private final String keyword;
+
+    public Emit(final int start, final int end ,final String keyword){
+        super(start,end);
+        this.keyword = keyword;
+    }
+
+    public String getKeyword(){return this.keyword;}
+
+    public String toString(){
+        return super.toString()+"="+this.keyword;
+    }
 }
