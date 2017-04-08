89d563cf02e0bb6949858675f94e5b2854bb1042
simon <seu_syh@163.com>
2017-04-08 13:09:19 +0800

    完成部分Trie的编写 Signed-off-by: simon <seu_syh@163.com>

--- src/main/java/segmentation/Trie/Trie.java
+++ src/main/java/segmentation/Trie/Trie.java
@@ -1,10 +1,10 @@
 package segmentation.Trie;
 
+import com.sun.org.apache.xalan.internal.xsltc.CollatorFactory;
 import com.sun.org.apache.xpath.internal.SourceTree;
+import segmentation.index.Interval;
 
-import java.util.HashSet;
-import java.util.Queue;
-import java.util.Set;
+import java.util.*;
 import java.util.concurrent.LinkedBlockingQueue;
 
 /**
@@ -47,7 +47,63 @@ public class Trie {
         return this;
     }
 
-    public void addEmits()
+    public void addEmits(){
+
+    }
+
+    public Collection<Emit> parseText(String text){
+        checkForConstructedFailureState();
+
+        int position = 0;
+        State currentState = this.rootState;
+        List<Emit> collectedEmits = new ArrayList<Emit>();
+        for(Character character:text.toCharArray()){
+            if(trieConfig.isCaseInsentiveWord()){
+                character = Character.toLowerCase(character);
+            }
+            currentState = getState(currentState,character);
+            storeEmits(position,currentState,collectedEmits);
+            ++position;
+        }
+        if(trieConfig.isOnlyWholeWord())
+        {
+            removePatialMatches(text,collectedEmits);
+        }
+        if(trieConfig.isAllowOverlap()){
+            IntervalTree intervalTree = new Interval()
+        }
+    }
+
+    public void removePatialMatches(String searchText,List<Emit> collectedEmits){
+        long size = searchText.length();
+        List<Emit> removeEmits = new ArrayList<Emit>();
+        for(Emit emit:collectedEmits){
+            if((emit.getStart()==0||!Character.isAlphabetic(searchText.charAt(emit.getStart()-1))&&
+                    (emit.getEnd()+1==size||!Character.isAlphabetic(searchText.charAt(emit.getEnd()+1)))))
+            {
+                continue;
+            }
+            removeEmits.add(emit);
+        }
+    }
+
+
+
+    public State getState(State currentState,Character character){
+        State newCurrentState = currentState.nextState(character);
+        while (newCurrentState==null){
+            currentState = currentState.getFailure();
+            newCurrentState = currentState.nextState(character);
+        }
+        return newCurrentState;
+    }
+
+    public void checkForConstructedFailureState(){
+        if(!this.failureStatesConstructed)
+        {
+            constructFailureStates();
+        }
+    }
 
     public void constructFailureStates(){
         Queue<State> queue = new LinkedBlockingQueue<State>();
@@ -59,7 +115,10 @@ public class Trie {
         //多线程时防止重新建立
         this.failureStatesConstructed = true;
 
-        //宽度优先遍历 深度>1的点建立failure表,bfs遍历，给每个节点添加对应的failure
+        /**
+         *  宽度优先遍历 深度>1的点建立failure表,给每个节点添加对应的failure
+         *  类似于KMP算法找公共前缀
+         */
         while (!queue.isEmpty()){
             State currentState = queue.remove();//当前层
             for(Character transition:currentState.getTransitions()){
@@ -75,4 +134,13 @@ public class Trie {
             }
         }
     }
+    public static void storeEmits(int position, State currentState, List<Emit> collectedEmits){
+        Collection<String> emits = currentState.getEmit();
+        if(emits!=null&&!emits.isEmpty()){
+            for(String emit:emits){
+                collectedEmits.add(new Emit(position-emit.length()+1,position,emit));
+            }
+        }
+    }
+
 }
